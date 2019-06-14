d59503db1c371ed48e2fc98ca6afe71a03d27713
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-14 15:11:06 -0500

    updated komodo/util.py to call get_request_metadata() for reading jsonrpc_ver and rpc_id

--- komodo/util.py
+++ komodo/util.py
@@ -8,54 +8,54 @@ def createmultisig(number_required=1, keys=['']):
     if (len(keys) > 0):
         key_list = key_list[:-1]
     key_list += "]";
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "createmultisig", "params": [' + str(
+    data = '{'+rpc.get_request_metadata()+', "method": "createmultisig", "params": [' + str(
         number_required) + ', ' + str(key_list) + '] }'
     return rpc.rpc_request(data)
 
 
 def decodeccopret(scriptPubKey=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "decodeccopret", "params": ["' + scriptPubKey + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "decodeccopret", "params": ["' + scriptPubKey + '"] }'
     return rpc.rpc_request(data)
 
 
 # The value -1.0 is returned if not enough transactions and blocks have been observed to make an estimate
 def estimatefee(num_blocks=0):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "estimatefee", "params": [' + str(num_blocks) + '] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "estimatefee", "params": [' + str(num_blocks) + '] }'
     return rpc.rpc_request(data)
 
 
 # The value -1.0 is returned if not enough transactions and blocks have been observed to make an estimate
 def estimatepriority(num_blocks=0):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "estimatepriority", "params": [' + str(num_blocks) + '] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "estimatepriority", "params": [' + str(num_blocks) + '] }'
     return rpc.rpc_request(data)
 
 
 def invalidateblock(block_hash=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "invalidateblock", "params": ["' + str(block_hash) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "invalidateblock", "params": ["' + str(block_hash) + '"] }'
     return rpc.rpc_request(data)
 
 
 def reconsiderblock(block_hash=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "reconsiderblock", "params": ["' + str(block_hash) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "reconsiderblock", "params": ["' + str(block_hash) + '"] }'
     return rpc.rpc_request(data)
 
 
 def txnotarizedconfirmed(tx_id=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "txnotarizedconfirmed", "params": ["' + str(tx_id) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "txnotarizedconfirmed", "params": ["' + str(tx_id) + '"] }'
     return rpc.rpc_request(data)
 
 
 def validateaddress(address=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "validateaddress", "params": ["' + str(address) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "validateaddress", "params": ["' + str(address) + '"] }'
     return rpc.rpc_request(data)
 
 
 def verifymessage(address='', signature='', message=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "verifymessage", "params": ["' + str(address) + '", "' + str(
+    data = '{'+rpc.get_request_metadata()+', "method": "verifymessage", "params": ["' + str(address) + '", "' + str(
         signature) + '", "' + str(message) + '"] }'
     return rpc.rpc_request(data)
 
 
 def z_validateaddress(address=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "z_validateaddress", "params": ["' + str(address) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "z_validateaddress", "params": ["' + str(address) + '"] }'
     return rpc.rpc_request(data)
