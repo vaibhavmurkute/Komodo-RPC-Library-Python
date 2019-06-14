b1bc2ba7b18ab5a606cb4699570b6915a4f43cb2
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-14 15:06:59 -0500

    updated komodo/network.py to call get_request_metadata() for reading jsonrpc_ver and rpc_id

--- komodo/network.py
+++ komodo/network.py
@@ -3,60 +3,60 @@ import rpc_util.rpc as rpc
 # node: ip:port
 # command: 'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once
 def addnode(node='', command=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "addnode", "params": ["' + str(node) + '", "' + str(command) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "addnode", "params": ["' + str(node) + '", "' + str(command) + '"] }'
     return rpc.rpc_request(data)
 
 
 def clearbanned():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "clearbanned", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "clearbanned", "params": [] }'
     return rpc.rpc_request(data)
 
 
 def disconnectnode(node=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "disconnectnode", "params": ["' + str(node) + '"] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "disconnectnode", "params": ["' + str(node) + '"] }'
     return rpc.rpc_request(data)
 
 
 # Nodes added via onetry are not listed here.
 def getaddednodeinfo(dns=False, node=''):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddednodeinfo", "params": [' + str(
+    data = '{'+rpc.get_request_metadata()+', "method": "getaddednodeinfo", "params": [' + str(
         dns).lower() + ', "' + str(node) + '"] }'
     return rpc.rpc_request(data)
 
 
 def getconnectioncount():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getconnectioncount", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "getconnectioncount", "params": [] }'
     return rpc.rpc_request(data)
 
 
 # This method is applicable only to the KMD main net.
 def getdeprecationinfo():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getdeprecationinfo", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "getdeprecationinfo", "params": [] }'
     return rpc.rpc_request(data)
 
 
 def getnettotals():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getnettotals", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "getnettotals", "params": [] }'
     return rpc.rpc_request(data)
 
 
 def getnetworkinfo():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getnetworkinfo", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "getnetworkinfo", "params": [] }'
     return rpc.rpc_request(data)
 
 
 def getpeerinfo():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getpeerinfo", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "getpeerinfo", "params": [] }'
     return rpc.rpc_request(data)
 
 
 def listbanned():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "listbanned", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "listbanned", "params": [] }'
     return rpc.rpc_request(data)
 
 
 def ping():
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "ping", "params": [] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "ping", "params": [] }'
     return rpc.rpc_request(data)
 
 
@@ -65,5 +65,5 @@ def ping():
 # bantime: indicates how long (in seconds) the ip is banned (or until when, if [absolute] is set). 0 or empty means the ban is using the default time of 24h, which can also be overwritten using the -bantime runtime parameter.
 # absolute: if set to true, the bantime must be an absolute timestamp (in seconds) since epoch (Jan 1 1970 GMT)
 def setban(ip='', command='', bantime=0, absolute=False):
-    data = '{"jsonrpc": "1.0", "id":"curltest", "method": "setban", "params": ["' + str(ip) + '", "' + str(command) + '", ' + str(bantime) + ', ' + str(absolute).lower() + '] }'
+    data = '{'+rpc.get_request_metadata()+', "method": "setban", "params": ["' + str(ip) + '", "' + str(command) + '", ' + str(bantime) + ', ' + str(absolute).lower() + '] }'
     return rpc.rpc_request(data)
