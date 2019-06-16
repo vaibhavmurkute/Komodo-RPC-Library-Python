ac32c26c39fea83e3aa1141adcf39c724c729f5f
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-16 14:15:26 -0500

    Adds docstrings to all functions in komodo/jumblr.py

--- komodo/jumblr.py
+++ komodo/jumblr.py
@@ -1,47 +1,39 @@
 import rpc_util.rpc as rpc
 
-'''
-    The jumblr_deposit method indicates the address from which Jumblr should withdraw funds.
-    There should be at least 10.024 KMD in this address
-        Arguments:
-            - depositaddress  : (string, required) : the address from which Jumblr will withdraw funds
-        Response:
-            (none)
-'''
 def jumblr_deposit(depositaddress=''):
+    '''
+    The jubmlr_deposit method indicates the address from which Jumblr should withdraw funds.
+    :param depositaddress: (string, required) the address from which Jumblr will withdraw funds
+    :return: JSON string
+    '''
     data = '{'+rpc.get_request_metadata()+', "method": "jumblr_deposit", "params": ["'+str(depositaddress)+'"] }'
     return rpc.rpc_request(data)
 
-'''
-    The jumblr_pause method instructs Jumblr to temporarily pause the privacy-shielding process.
-        Arguments:
-            (none)
-        Response:
-            (none)
-'''
+
 def jumblr_pause():
+    '''
+    The jumblr_pause method instructs Jumblr to temporarily pause the privacy-shielding process.
+    :return: JSON string
+    '''
     data = '{'+rpc.get_request_metadata()+', "method": "jumblr_pause", "params": [] }'
     return rpc.rpc_request(data)
 
-'''
-    The jumblr_resume method instructs Jumblr to resume the privacy-shielding process.
-        Arguments:
-            (none)
-        Response:
-            (none)
-'''
+
 def jumblr_resume():
+    '''
+    The jumblr_resume method instructs Jumblr to resume the privacy-shielding process.
+    :return: JSON string
+    '''
     data = '{'+rpc.get_request_metadata()+', "method": "jumblr_resume", "params": [] }'
     return rpc.rpc_request(data)
 
-'''
-    The jumblr_secret method indicates to Jumblr the final t destination address.
-        Arguments:
-            - secretaddress  : (string, required) : the destination transparent address
-        Response:
-            (none)
-'''
+
 def jumblr_secret(secretaddress=''):
+    '''
+    The jumblr_secret method indicates to Jumblr the final t destination address.
+    :param secretaddress: (string, required) the destination transparent address
+    :return: JSON string
+    '''
     data = '{'+rpc.get_request_metadata()+', "method": "jumblr_secret", "params": ["'+str(secretaddress)+'"] }'
     return rpc.rpc_request(data)
 
