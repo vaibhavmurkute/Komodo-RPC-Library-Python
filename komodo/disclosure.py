bd17244d92446076b3e2575bfc7dc098612da448
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-16 02:03:33 -0500

    Adds docstrings to all functions in komodo/disclosure.py

--- komodo/disclosure.py
+++ komodo/disclosure.py
@@ -1,27 +1,25 @@
 import rpc_util.rpc as rpc
 
-'''
-    z_getpaymentdisclosure method generates a payment disclosure for a given joinsplit output.
-        Arguments:
-            - txid          : (string, required)
-            - js_index      :  (string, required)
-            - output_index  : (string, required)
-            - message       : (string, optional)
-        Response:
-            - paymentdisclosure : (string) : a hex data string, with a "zpd:" prefix
-'''
 def z_getpaymentdisclosure(txid='', js_index='', output_index='', message=''):
+    '''
+    The z_getpaymentdisclosure method generates a payment disclosure for a given joinsplit output
+    EXPERIMENTAL FEATURE: Payment disclosure is currently DISABLED. This call always fails.
+    :param txid: (string, required)
+    :param js_index: (string, required)
+    :param output_index: (string, required)
+    :param message: (string, optional)
+    :return: "paymentdisclosure"	(string)	a hex data string, with a "zpd:" prefix
+    '''
     data = '{'+rpc.get_request_metadata()+', "method": "z_getpaymentdisclosure", "params": ["'+str(txid)+'","'+str(js_index)+'","'+str(output_index)+'","'+str(message)+'"] }'
     return rpc.rpc_request(data)
 
-'''
-    The z_validatepaymentdisclosure method validates a payment disclosure.
-        Arguments:
-            - paymentdisclosure : (string, required): hex data string, with "zpd:" prefix
-        Response:
-            (currently disabled)		
-'''
+
 def z_validatepaymentdisclosure(paymentdisclosure=''):
+    '''
+    The z_validatepaymentdisclosure method validates a payment disclosure.
+    :param paymentdisclosure: (string, required)	hex data string, with "zpd:" prefix
+    :return: (currently disabled)
+    '''
     data = '{'+rpc.get_request_metadata()+', "method": "z_validatepaymentdisclosure", "params": ["'+str(paymentdisclosure)+'"] }'
     return rpc.rpc_request(data)
 
