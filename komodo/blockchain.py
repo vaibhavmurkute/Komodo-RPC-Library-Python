d34bc6be60183fc2e70055019e5d5988836827ba
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-16 02:23:09 -0500

    refactored function docstrings

--- komodo/blockchain.py
+++ komodo/blockchain.py
@@ -6,23 +6,25 @@ def coinsupply(height=0):
     :param height: (integer, optional)	the desired block height
     :return: JSON string containing:
         "result"	(string)	whether the request was successful
-        "coin"	(string)	the ticker symbol of the coin for asset chains, otherwise KMD
+        "coin"      (string)	the ticker symbol of the coin for asset chains, otherwise KMD
         "height"	(integer)	the height of this coin supply data
-        "supply"	(float)	the transparent coin supply
-        "zfunds"	(float)	the shielded coin supply (in zaddrs)
-        "sprout"	(float)	the sprout coin supply (in zcaddrs)
-        "total"	(float)	the total coin supply, i.e. sum of supply + zfunds
+        "supply"	(float)	    the transparent coin supply
+        "zfunds"	(float)	    the shielded coin supply (in zaddrs)
+        "sprout"	(float)	    the sprout coin supply (in zcaddrs)
+        "total"	    (float)	    the total coin supply, i.e. sum of supply + zfunds
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "coinsupply", "params": [' + (str(height) if height > 1 else '') + '] }'
     return rpc.rpc_request(data)
 
+
 def getbestblockhash():
     '''
     The getbestblockhash method returns the hash of the best (tip) block in the longest block chain
-    :return: "hex"	(JSON string)	the block hash, hex encoded
+    :return: "hex"  (JSON string)   he block hash, hex encoded
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "getbestblockhash", "params": [] }'
     return rpc.rpc_request(data)
+
 
 def getblock(block_id=1, verbose=True):
     '''
@@ -30,23 +32,24 @@ def getblock(block_id=1, verbose=True):
     :param block_id: (string OR number) block_id can be block-hash (string) OR block-height (number)
     :param verbose: (boolean, optional, default=true)	true returns a json object, false returns hex-encoded data
     :return: JSON string containing:
-        "hash"	(string)	the block hash (same as provided hash)
-        "confirmations"	(numeric)	a confirmation number that is dPoW aware;
+        "hash"	            (string)	the block hash (same as provided hash)
+        "confirmations"	    (numeric)	a confirmation number that is dPoW aware;
         "rawconfirmations"	(numeric)	the raw confirmations (number of blocks on top of this block); the returned value is -1 if the block is not on the main chain
-        "size"	(numeric)	the block size
-        "height"	(numeric)	the block height or index (same as provided height)
-        "version"	(numeric)	the block version
-        "merkleroot"	(string)	the merkle root
-        "tx" : [ "transaction_id" ,...]	(array of strings)
-        "time"	(numeric)	the block time in seconds since epoch (Jan 1 1970 GMT)
-        "nonce"	(numeric)	the nonce
-        "bits"	(string)	the bits
-        "difficulty"	(numeric)	the difficulty
+        "size"	            (numeric)	the block size
+        "height"	        (numeric)	the block height or index (same as provided height)
+        "version"	        (numeric)	the block version
+        "merkleroot"	    (string)	the merkle root
+        "tx" :          [ "transaction_id" ,...]	(array of strings)
+        "time"	            (numeric)	the block time in seconds since epoch (Jan 1 1970 GMT)
+        "nonce"	            (numeric)	the nonce
+        "bits"	            (string)	the bits
+        "difficulty"	    (numeric)	the difficulty
         "previousblockhash"	(string)	the hash of the previous block
-        "nextblockhash"	(string)	the hash of the next block
+        "nextblockhash"	    (string)	the hash of the next block
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "getblock", "params": ["'+str(block_id)+'", '+str(verbose).lower()+'] }'
     return rpc.rpc_request(data)
+
 
 def getblockchaininfo():
     '''
@@ -56,6 +59,7 @@ def getblockchaininfo():
     data = '{'+rpc.get_request_metadata()+', "method": "getblockchaininfo", "params": [] }'
     return rpc.rpc_request(data)
 
+
 def getblockcount():
     '''
     The getblockcount method returns the number of blocks in the best valid block chain
@@ -63,6 +67,7 @@ def getblockcount():
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "getblockcount", "params": [] }'
     return rpc.rpc_request(data)
+
 
 def getblockhash(index=1):
     '''
@@ -73,13 +78,7 @@ def getblockhash(index=1):
     data = '{'+rpc.get_request_metadata()+', "method": "getblockhash", "params": ['+str(index)+'] }'
     return rpc.rpc_request(data)
 
-'''
-    getblockhashes(high, low, no_orphans, logical_times):
-        - high: (numeric, required, default=0) : the newer block timestamp
-        - low: (numeric, required, default=0) : the older block timestamp
-        - no_orphans (boolean, required, default=False) : a value of true implies that the method will only include blocks on the main chain
-        - logical_times (boolean, required, default=False) : a value of true implies that the method will only include logical timestamps with hashes
-'''
+
 def getblockhashes(high=0, low=0, no_orphans=False, logical_times=False):
     '''
     The getblockhashes method returns an array of hashes of blocks within the timestamp range provided.
@@ -89,7 +88,7 @@ def getblockhashes(high=0, low=0, no_orphans=False, logical_times=False):
     :param no_orphans: (boolean)	a value of true implies that the method will only include blocks on the main chain
     :param logical_times: (boolean)	a value of true implies that the method will only include logical timestamps with hashes
     :return: JSON string containing:
-        "hash"	(string)	the block hash
+        "hash"	    (string)	the block hash
         "blockhash"	(string)	the block hash
         "logicalts"	(numeric)	the logical timestamp
     '''
@@ -100,7 +99,7 @@ def getblockhashes(high=0, low=0, no_orphans=False, logical_times=False):
 def getblockheader(hash='', verbose=True):
     '''
     The getblockheader method returns information about the indicated block.
-    :param hash: (string, required)	the block hash
+    :param hash:    (string, required)	the block hash
     :param verbose: (boolean, optional, default=true)	true returns a json object, false returns hex-encoded data
     :return: JSON string containing information about the indicated block
     '''
@@ -112,11 +111,11 @@ def getchaintips():
     The getchaintips method returns information about all known tips in the block tree, including the main chain and any orphaned branches.
     :return: JSON string containing:
         "height"	(numeric)	the height of the chain tip
-        "hash"	(string)	the block hash of the tip
+        "hash"	    (string)	the block hash of the tip
         "branchlen"	(numeric)	0 for main chain
         "status"	(string)	"active" for the main chain
         "height"	(numeric)	the height of the branch tip
-        "hash"	(string)	the blockhash of the branch tip
+        "hash"	    (string)	the blockhash of the branch tip
         "branchlen"	(numeric)	the length of the branch connecting the tip to the main chain
         "status"	(string)	the status of the chain
     '''
@@ -129,13 +128,13 @@ def getchaintxstats(nblocks=1, blockhash=''):
     :param nblocks: (numeric, optional)	the number of blocks in the averaging window.
     :param blockhash: (string, optional)	the hash of the block which ends the window
     :return: JSON string containing:
-        "time"	(numeric)	the timestamp for the final block in the window in UNIX format
-        "txcount"	(numeric)	the total number of transactions in the chain up to this point
+        "time"	                    (numeric)	the timestamp for the final block in the window in UNIX format
+        "txcount"	                (numeric)	the total number of transactions in the chain up to this point
         "window_final_block_hash"	(string)	the hash of the final block in the window
-        "window_block_count"	(numeric)	the size of the window in the number of blocks
-        "window_tx_count"	(numeric)	the number of transactions in the window; this value is only returned if window_block_count is > 0.
-        "window_interval"	(numeric)	the elapsed time in the window in seconds; this value is only returned if window_block_count is > 0.
-        "txrate"	(numeric)	the average rate of transactions per second in the window; this value is only returned if window_interval is > 0
+        "window_block_count"	    (numeric)	the size of the window in the number of blocks
+        "window_tx_count"	        (numeric)	the number of transactions in the window; this value is only returned if window_block_count is > 0.
+        "window_interval"	        (numeric)	the elapsed time in the window in seconds; this value is only returned if window_block_count is > 0.
+        "txrate"	                (numeric)	the average rate of transactions per second in the window; this value is only returned if window_interval is > 0
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "getchaintxstats", "params": ['+str(nblocks)+', "'+str(blockhash)+'"] }'
     return rpc.rpc_request(data)
@@ -155,10 +154,10 @@ def getlastsegidstakes(depth=1):
     :param depth: (numeric, required)	the number of blocks to scan, starting from the current height and working backwards.
     :return: JSON string containing:
         "NotSet"	(numeric)	the number of blocks that have no SegId set
-        "PoW"	(numeric)	the number of blocks created through PoW
+        "PoW"	    (numeric)	the number of blocks created through PoW
         "PoSPerc"	(numeric)	the percentage of blocks created through PoS
         "SegIds"	(json object)	the json containing the data of number of blocks in each SegId
-        "n"	(numeric)	the number of blocks staked from SegId n in the last X blocks, where X is equal to the indicated depth
+        "n"	        (numeric)	the number of blocks staked from SegId n in the last X blocks, where X is equal to the indicated depth
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "getlastsegidstakes", "params": ['+str(depth)+'] }'
     return rpc.rpc_request(data)
@@ -180,14 +179,14 @@ def getrawmempool(verbose=False):
     :param verbose: (boolean, optional, default=false)	true for a json object, false for a json array of transaction ids
     :return: JSON string, if verbose=False: "transaction_id"	(string)	the transaction id
         if verbose=True:
-        "transaction_id": { .... }	(json object)
-        "size"	(numeric)	the transaction size in bytes
-        "fee"	(numeric)	the transaction fee
-        "time"	(numeric)	the local time transaction entered pool in seconds since 1 Jan 1970 GMT
-        "height"	(numeric)	the block height wherein the transaction entered the mempool
+        "transaction_id":   { .... }	(json object)
+        "size"	            (numeric)	the transaction size in bytes
+        "fee"	            (numeric)	the transaction fee
+        "time"	            (numeric)	the local time transaction entered pool in seconds since 1 Jan 1970 GMT
+        "height"	        (numeric)	the block height wherein the transaction entered the mempool
         "startingpriority"	(numeric)	the priority when the transaction entered the mempool
         "currentpriority"	(numeric)	the transaction priority at the current height
-        "depends": { ... }	(array)	unconfirmed transactions used as inputs for this transaction
+        "depends": { ... }	(array)	    unconfirmed transactions used as inputs for this transaction
         "transaction_id"	(string)	the parent transaction id
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "getrawmempool", "params": ['+str(verbose).lower()+'] }'
@@ -237,13 +236,13 @@ def gettxoutsetinfo():
     '''
     The gettxoutsetinfo method returns statistics about the unspent transaction output set
     :return: JSON string containing:
-        "height"	(numeric)	the current block height (index)
-        "bestblock"	(string)	the best block hash hex
-        "transactions"	(numeric)	the number of transactions
-        "txouts"	(numeric)	the number of output transactions
+        "height"	        (numeric)	the current block height (index)
+        "bestblock"	        (string)	the best block hash hex
+        "transactions"	    (numeric)	the number of transactions
+        "txouts"	        (numeric)	the number of output transactions
         "bytes_serialized"	(numeric)	the serialized size
         "hash_serialized"	(string)	the serialized hash
-        "total_amount"	(numeric)	the total amount
+        "total_amount"	    (numeric)	the total amount
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "gettxoutsetinfo", "params": [] }'
     return rpc.rpc_request(data)
@@ -253,16 +252,16 @@ def kvsearch(key=''):
     The kvsearch method searches for a key stored via the kvupdate command.
     :param key: (string, required)	the key for which the user desires to search the chain
     :return: JSON string containing:
-        "coin"	(string)	the chain on which the key is stored
+        "coin"	        (string)	the chain on which the key is stored
         "currentheight"	(numeric)	the current height of the chain
-        "key"	(string)	the key
-        "keylen"	(string)	the length of the key
-        "owner"	(string)	a hex string representing the owner of the key
-        "height"	(numeric)	the height at which the key was stored
+        "key"	        (string)	the key
+        "keylen"	    (string)	the length of the key
+        "owner"	        (string)	a hex string representing the owner of the key
+        "height"	    (numeric)	the height at which the key was stored
         "expiration"	(numeric)	the height at which the key will expire
-        "flags"	(numeric)	1 if the key was created with a password; 0 otherwise
-        "value"	(string)	the stored value
-        "valuesize"	(string)	the amount of characters stored
+        "flags"	        (numeric)	1 if the key was created with a password; 0 otherwise
+        "value"	        (string)	the stored value
+        "valuesize"	    (string)	the amount of characters stored
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "kvsearch", "params": ["'+str(key)+'"] }'
     return rpc.rpc_request(data)
@@ -274,16 +273,16 @@ def kvupdate(key='', value='', days=1, passphrase=''):
     :param value: (string, required)	value
     :param days: (numeric, required)	amount of days before the key expires (1440 blocks/day); minimum 1 day
     :param passphrase: (string, optional)	passphrase required to update this key
-    :return: "coin"	(string)	the chain on which the key is stored
-        "height"	(numeric)	the height at which the key was stored
+    :return: "coin"	    (string)	the chain on which the key is stored
+        "height"	    (numeric)	the height at which the key was stored
         "expiration"	(numeric)	the height at which the key will expire
-        "flags"	(string)	the amount of days the key will be stored
-        "key"	(numeric)	the stored key
-        "keylen"	(numeric)	the length of the key
-        "value"	(numeric)	the stored value
-        "valuesize"	(string)	the length of the stored value
-        "fee"	(string)	the transaction fee paid to store the key
-        "txid"	(string)	the transaction id
+        "flags"	        (string)	the amount of days the key will be stored
+        "key"	        (numeric)	the stored key
+        "keylen"	    (numeric)	the length of the key
+        "value"	        (numeric)	the stored value
+        "valuesize"	    (string)	the length of the stored value
+        "fee"	        (string)	the transaction fee paid to store the key
+        "txid"	        (string)	the transaction id
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "kvupdate", "params": ["'+str(key)+'","'+str(value)+'","'+str(days)+'", "'+str(passphrase)+'"] }'
     return rpc.rpc_request(data)
@@ -294,10 +293,10 @@ def minerids(height=1):
     :param height: (number)	the block height for the query
     :return: JSON string containing:
         "mined":
-        "notaryid"	(number)	the id of the specific notary node
+        "notaryid"	    (number)	the id of the specific notary node
         "kmdaddress"	(string)	the address of the notary node
-        "pubkey"	(string)	the public signing key of the notary node
-        "blocks"	(number)
+        "pubkey"	    (string)	the public signing key of the notary node
+        "blocks"	    (number)
     '''
     data = '{'+rpc.get_request_metadata()+', "method": "minerids", "params": ["'+str(height)+'"] }'
     return rpc.rpc_request(data)
@@ -309,13 +308,13 @@ def notaries(height=0, timestamp=0):
     :param height: (number)	the block height desired for the query
     :param timestamp: (number)	the timestamp of the block desired for the query
     :return: JSON string containing:
-        "notaries": [ ... ]	(array)
-        "pubkey"	(string)	the public signing key of the indicated notary node, used on the KMD network to create notary-node authorized transactions
+        "notaries":     [ ... ]	(array)
+        "pubkey"	    (string)	the public signing key of the indicated notary node, used on the KMD network to create notary-node authorized transactions
         "BTCaddress"	(string)	the public BTC address the notary node uses on the BTC blockchain to create notarizations
         "KMDaddress"	(string)	the public KMD address the notary node uses on the KMD blockchain to create notarizations
         "numnotaries"	(number)	the number of notary nodes; typically this value is 64, but the value may vary on rare circumstances, such as during election seasons
-        "height"	(number)	the block height number at which the notary-node information applies
-        "timestamp"	(number)	the timestamp at which the notary-node information applies
+        "height"	    (number)	the block height number at which the notary-node information applies
+        "timestamp"	    (number)	the timestamp at which the notary-node information applies
     '''
     options = ('"'+str(height)+'", "'+str(timestamp)+'"' if(height != 0 and timestamp != 0) else('"'+str(height)+'"' if height > 0 else(('"'+str(timestamp)+'"' if timestamp > 0 else ''))))
     data = '{'+rpc.get_request_metadata()+', "method": "notaries", "params": ['+options+'] }'
