e12965beeb067686299a16530501a0c7358ec57b
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-17 22:28:28 -0500

    Moved options: jsonrpc_ver and rpc_req_id to komodo_rpc module 	- modified code to read these values from komodo_rpc

---
+++ rpc_util/rpc.py
@@ -0,0 +1,15 @@
+import requests
+from komodo_rpc import KomodoRpc
+
+def rpc_request(data_string=''):
+    response = requests.post(KomodoRpc.req_url, headers=KomodoRpc.req_headers, data=data_string,
+                             auth=(KomodoRpc.rpc_username, KomodoRpc.rpc_password))
+    if (response.ok):
+        return response.text
+    else:
+        return 'Failed with Status Code: ' + str(response.status_code) + ', reason: ' + str(response.text)
+
+def get_request_metadata():
+    return '"jsonrpc": "'+str(KomodoRpc.jsonrpc_ver)+'", "id":"'+str(KomodoRpc.rpc_req_id)+'"'
+
+
