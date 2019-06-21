fe56e5b65d625f1fb71cf48b8ed6c21fc85c2f76
Vaibhav Murkute <vaibhavmurkute88@gmail.com>
2019-06-20 21:52:13 -0500

    refactored code to keep characters-per-line count to 79 (PEP-8 Code-Style standard)         - code: 79 chars per line         - docstrings: 72 chars per line

--- komodo/disclosure.py
+++ komodo/disclosure.py
@@ -2,25 +2,34 @@ import rpc_util.rpc as rpc
 
 def z_getpaymentdisclosure(txid='', js_index='', output_index='', message=''):
     '''
-    The z_getpaymentdisclosure method generates a payment disclosure for a given joinsplit output
-    EXPERIMENTAL FEATURE: Payment disclosure is currently DISABLED. This call always fails.
+    The z_getpaymentdisclosure method generates a payment disclosure
+    for a given joinsplit output
+    EXPERIMENTAL FEATURE: Payment disclosure is currently DISABLED.
+    This call always fails.
     :param txid: (string, required)
     :param js_index: (string, required)
     :param output_index: (string, required)
     :param message: (string, optional)
-    :return: "paymentdisclosure" (string) a hex data string, with a "zpd:" prefix
+    :return: "paymentdisclosure" (string) a hex data string,
+        with a "zpd:" prefix
     '''
-    data = '{'+rpc.get_request_metadata()+', "method": "z_getpaymentdisclosure", "params": ["'+str(txid)+'","'+str(js_index)+'","'+str(output_index)+'","'+str(message)+'"] }'
+    data = '{'+rpc.get_request_metadata()+', ' \
+                '"method": "z_getpaymentdisclosure", ' \
+                '"params": ["'+str(txid)+'","'+str(js_index)+\
+                    '","'+str(output_index)+'","'+str(message)+'"] }'
     return rpc.rpc_request(data)
 
 
 def z_validatepaymentdisclosure(paymentdisclosure=''):
     '''
     The z_validatepaymentdisclosure method validates a payment disclosure.
-    :param paymentdisclosure: (string, required) hex data string, with "zpd:" prefix
+    :param paymentdisclosure: (string, required) hex data string,
+        with "zpd:" prefix
     :return: (currently disabled)
     '''
-    data = '{'+rpc.get_request_metadata()+', "method": "z_validatepaymentdisclosure", "params": ["'+str(paymentdisclosure)+'"] }'
+    data = '{'+rpc.get_request_metadata()+', ' \
+                '"method": "z_validatepaymentdisclosure", ' \
+                '"params": ["'+str(paymentdisclosure)+'"] }'
     return rpc.rpc_request(data)
 
 
